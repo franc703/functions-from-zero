a83cdd0e127f56b6991fe38850dc1efa6a8d7722
Rodrigo Franco <49168245+franc703@users.noreply.github.com>
2023-05-30 19:50:48 +0000

    Adding test for cli

--- wikibot.py
+++ wikibot.py
@@ -1,7 +1,15 @@
-import wikipedia
+import click
+from mylib.bot import scrape
 
-def scrape(name="Microsoft", length=1):
-    result = wikipedia.summary(name, sentences = length)
-    return result
+@click.command()
+@click.option("--name", prompt="Wikipedia page to scrape", 
+              help="Webpage we want to scrape")
+@click.option("--length", 
+              help="The length of the output from Wikipedia")
+def cli(name, length):
+    result = scrape(name, length=length)
+    click.echo(click.style(f"{result}:", bg="blue", fg="white"))
+    
 
-print(scrape())
+if __name__ == '__main__':
+    cli()
